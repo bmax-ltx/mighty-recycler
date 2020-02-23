7d77050adf3e5d2eba0aeef2f19672c27f905d0e
bmax <bmaximenko@lightricks.com>
2020-02-23 18:22:07 +0200

    Create Staggered Grid Activity

---
+++ app/src/main/java/com/lightricks/mightyrecycler/staggered/StaggeredGridActivity.java
@@ -0,0 +1,39 @@
+package com.lightricks.mightyrecycler.staggered;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.lightricks.mightyrecycler.R;
+import com.lightricks.mightyrecycler.databinding.ActivityStaggeredGridBinding;
+import com.lightricks.mightyrecycler.model.MaterialColors;
+
+public class StaggeredGridActivity extends AppCompatActivity {
+    private ActivityStaggeredGridBinding dataBinding;
+
+    @Override
+    protected void onCreate(Bundle state) {
+        super.onCreate(state);
+
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_staggered_grid);
+        setupViews();
+    }
+
+    private void setupViews() {
+        dataBinding.recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,
+                StaggeredGridLayoutManager.VERTICAL));
+        dataBinding.recyclerView.setHasFixedSize(true);
+        dataBinding.recyclerView.setAdapter(makeAdapter());
+    }
+
+    private RecyclerView.Adapter makeAdapter() {
+        StaggeredGridAdapter adapter = new StaggeredGridAdapter();
+        MaterialColors colors = new MaterialColors();
+        adapter.setColors(colors.getRandomColors(150));
+        return adapter;
+    }
+
+}
